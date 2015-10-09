3389dee99f95c10e3791db2f9599cb5cf2067e21
afi031 <afi.duapuluh@gmail.com>
2015-10-10 05:45:48 +0700

    MI2E_23_Tugas-2015-02-afi031
    Tugas 2

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,200 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtTampil = New System.Windows.Forms.TextBox()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnSamaDengan = New System.Windows.Forms.Button()
+        Me.btnJumlah = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btnKurang = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtTampil
+        '
+        Me.txtTampil.Location = New System.Drawing.Point(10, 9)
+        Me.txtTampil.Name = "txtTampil"
+        Me.txtTampil.ReadOnly = True
+        Me.txtTampil.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.txtTampil.Size = New System.Drawing.Size(202, 20)
+        Me.txtTampil.TabIndex = 0
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(10, 164)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(46, 37)
+        Me.btn0.TabIndex = 1
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnSamaDengan
+        '
+        Me.btnSamaDengan.Location = New System.Drawing.Point(62, 164)
+        Me.btnSamaDengan.Name = "btnSamaDengan"
+        Me.btnSamaDengan.Size = New System.Drawing.Size(97, 37)
+        Me.btnSamaDengan.TabIndex = 2
+        Me.btnSamaDengan.Text = "="
+        Me.btnSamaDengan.UseVisualStyleBackColor = True
+        '
+        'btnJumlah
+        '
+        Me.btnJumlah.Location = New System.Drawing.Point(165, 164)
+        Me.btnJumlah.Name = "btnJumlah"
+        Me.btnJumlah.Size = New System.Drawing.Size(46, 37)
+        Me.btnJumlah.TabIndex = 3
+        Me.btnJumlah.Text = "+"
+        Me.btnJumlah.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(10, 121)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(46, 37)
+        Me.btn1.TabIndex = 4
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(62, 121)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(46, 37)
+        Me.btn2.TabIndex = 5
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(113, 121)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(46, 37)
+        Me.btn3.TabIndex = 6
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(10, 78)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(46, 37)
+        Me.btn4.TabIndex = 7
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(62, 78)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(46, 37)
+        Me.btn5.TabIndex = 8
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(114, 78)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(46, 37)
+        Me.btn6.TabIndex = 9
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(10, 35)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(46, 37)
+        Me.btn7.TabIndex = 10
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(62, 35)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(46, 37)
+        Me.btn8.TabIndex = 11
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(114, 35)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(46, 37)
+        Me.btn9.TabIndex = 12
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btnKurang
+        '
+        Me.btnKurang.Location = New System.Drawing.Point(165, 121)
+        Me.btnKurang.Name = "btnKurang"
+        Me.btnKurang.Size = New System.Drawing.Size(46, 37)
+        Me.btnKurang.TabIndex = 13
+        Me.btnKurang.Text = "-"
+        Me.btnKurang.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(166, 35)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(46, 80)
+        Me.btnClear.TabIndex = 14
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(220, 207)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnKurang)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnJumlah)
+        Me.Controls.Add(Me.btnSamaDengan)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.txtTampil)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents txtTampil As System.Windows.Forms.TextBox
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnSamaDengan As System.Windows.Forms.Button
+    Friend WithEvents btnJumlah As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btnKurang As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
 
 End Class
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,144 @@
 ﻿Public Class Form1
 
+    Dim pilih As String ' untuk select case
+    Dim input As Integer
+    Dim angka As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtTampil.Text = "0"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        If txtTampil.Text = "0" Then
+            angka = "0"
+        Else
+            angka &= "0"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If txtTampil.Text = "0" Then
+            angka = "1"
+        Else
+            angka &= "1"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If txtTampil.Text = "0" Then
+            angka = "2"
+        Else
+            angka &= "2"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If txtTampil.Text = "0" Then
+            angka = "3"
+        Else
+            angka &= "3"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If txtTampil.Text = "0" Then
+            angka = "4"
+        Else
+            angka &= "4"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If txtTampil.Text = "0" Then
+            angka = "5"
+        Else
+            angka &= "5"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If txtTampil.Text = "0" Then
+            angka = "6"
+        Else
+            angka &= "6"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If txtTampil.Text = "0" Then
+            angka = "7"
+        Else
+            angka &= "7"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If txtTampil.Text = "0" Then
+            angka = "8"
+        Else
+            angka &= "8"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If txtTampil.Text = "0" Then
+            angka = "9"
+        Else
+            angka &= "9"
+        End If
+        txtTampil.Text = angka
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtTampil.Text = "0"
+        angka = Nothing
+        Me.input = 0
+    End Sub
+
+    Private Sub btnSamaDengan_Click(sender As Object, e As EventArgs) Handles btnSamaDengan.Click
+        Select Case Me.pilih
+            Case "-"
+                txtTampil.Text = Convert.ToString(Me.input - Convert.ToInt32(txtTampil.Text))
+            Case "+"
+                txtTampil.Text = Convert.ToString(Me.input + Convert.ToInt32(txtTampil.Text))
+        End Select
+        Me.input = 0 'meriset variabel input menjadi 0
+        'Convert.ToInt32(txtTampil.Text) : angka terakhir setelah kita menekan =
+    End Sub
+
+    Private Sub btnKurang_Click(sender As Object, e As EventArgs) Handles btnKurang.Click
+        Me.pilih = "-"
+        If (Me.input = 0) Then
+            Me.input += Convert.ToInt32(txtTampil.Text)
+        Else
+            Me.input -= Convert.ToInt32(txtTampil.Text)
+        End If
+        txtTampil.Text = Convert.ToString(Me.input)
+        angka = Nothing
+
+        'If (Me.input = 0) Then
+        'Me.input += Convert.ToInt32(txtTampil.Text)
+        'Jika tidak ada ini, aksi yg di lakukan penambahan
+        'contoh : 0-1-2 = -3 , yg tepat 0+1-2 = -1
+    End Sub
+
+    Private Sub btnJumlah_Click(sender As Object, e As EventArgs) Handles btnJumlah.Click
+        Me.pilih = "+"
+        Me.input += Convert.ToInt32(txtTampil.Text)
+        txtTampil.Text = Convert.ToString(Me.input)
+        angka = Nothing
+    End Sub
+
+    'Me.input += Convert.ToInt32(txtTampil.Text) atau 
+    'Me.input -= Convert.ToInt32(txtTampil.Text)
+    'menjumlah atau mengurangi angka ketika menekan - atau + (belum menekan =)
 End Class
